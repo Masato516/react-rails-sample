b168afd30fee5e2e0ac22e6330085cf8b317e7a4
Masato516 <yaginen9@gmail.com>
2021-03-31 06:34:37 +0000

    until PART 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: redirect('/todos')
+
+  get 'todos', to: 'site#index'
+  get 'todos/new', to: 'site#index'
+  get 'todos/:id/edit', to: 'site#index'
+  
+  # react で http 通信を行うためのルーティング
+  namespace :api do
+    namespace :v1 do
+      # resources にないアクションを使っているため別途で作成
+      delete '/todos/destroy_all', to: 'todos#destroy_all'
+      # edit, newアクションは定義していないためonlyで制約
+      resources :todos, only: %i[index show create update destroy]
+    end
+  end
 end
